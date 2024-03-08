ac17a3ffc7b4f226a583275561babe5a80599ebe
jcas96 <162757478+jcas96@users.noreply.github.com>
2024-03-08 14:41:56 -0800

    Update ShoppingTimeRemaining.java

--- ShoppingTimeRemaining.java
+++ ShoppingTimeRemaining.java
@@ -1,4 +1,6 @@
 public interface ShoppingTimeRemaining {
+
+	//Creates 2 abstract methods to be used in further classes
 	abstract void setShoppingTimeRemaining(int value);
 	abstract int getShoppingTimeRemaining();
 }
