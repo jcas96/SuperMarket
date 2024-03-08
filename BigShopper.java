a2a9996fb5a36e1ebbfb5b4574ae13ea74fdce32
jcas96 <162757478+jcas96@users.noreply.github.com>
2024-03-08 14:50:08 -0800

    Update BigShopper.java

--- BigShopper.java
+++ BigShopper.java
@@ -1,6 +1,9 @@
 import java.util.Random;
 
 public class BigShopper extends Shopper{
+	/creates a subclass of shopper so all the abstract methods
+	//from Shopper.java and ShoppingTimeRemaining.java will be implemented
+	public static int fastShopperCounter =0;
 	public static int bigShopperCounter =0;
 	public static int TIMEWITHCHECKER=2;
 	private String bigShopperID;
@@ -63,7 +66,9 @@ public class BigShopper extends Shopper{
 	}
 	
 	Random randy;
-	
+	//BigShopper Constructor that sets the type of shopper through the Shopper super class
+	//and using the already instantiated bigShopperCounter you create a random obect
+	//then ID, time, timeshopping, and time remaining are set 
 	BigShopper(int input){
 		super("BigShopper");
 		randy = new Random(bigShopperCounter);
@@ -77,7 +82,8 @@ public class BigShopper extends Shopper{
 		bigShopperCounter++;
 		bigShopperID =(getShopperType()+bigShopperCounter);
 	}
-	
+
+	//uses the randy object created in the constructor to set the time shopping from 1-6
 	public void setTimeShopping() {
 		timeShopping = randy.nextInt(5,16);
 	}
@@ -86,14 +92,15 @@ public class BigShopper extends Shopper{
 		timeOutOfCheckoutLine = out;
 		calculateFinalDurations();
 	}
-	
+
+	//calculates how long a shopper spent inside the store
 	public void calculateFinalDurations() {
 		endTime = timeOutOfCheckoutLine + TIMEWITHCHECKER;
 		totalTimeCheckingOut = Math.abs(endTime-timeIntoCheckoutLine);
 		totalTimeInStore = Math.abs(endTime-startTime);
 	}
 
-	
+	//uses String.format format to print out the values from BigShopper
 	@Override
 	public String toString(){
 		return String.format("%-22S%-11d%-16d%-16d%-16d%-16d", bigShopperID,startTime,endTime,timeShopping,totalTimeCheckingOut,totalTimeInStore);
